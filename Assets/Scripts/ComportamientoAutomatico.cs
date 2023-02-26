5af9b11937d20a0cf2c33ab97d04b83e7992b0ee
JesusHaans <jesuslopez@ciencias.unam.mx>
2023-02-25 21:03:20 -0600

    Añadiendo Giro Bonito de Dron

--- Assets/Scripts/ComportamientoAutomatico.cs
+++ Assets/Scripts/ComportamientoAutomatico.cs
@@ -6,10 +6,19 @@ public class ComportamientoAutomatico : MonoBehaviour {
 
 	private Sensores sensor;
 	private Actuadores actuador;
+	public Vector3 baseDeCarga = Vector3.zero;
+	public Transform uBase;
+	public Transform baseCarga;
+	int grados = 0;
+	bool girando = false;
+	bool dir = true; 
+
 
 	void Start(){
 		sensor = GetComponent<Sensores>();
 		actuador = GetComponent<Actuadores>();
+		uBase = GameObject.Find("Base").transform;
+		baseCarga = this.transform;
 	}
 
 	void FixedUpdate () {
@@ -17,12 +26,35 @@ public class ComportamientoAutomatico : MonoBehaviour {
 			return;
 		}
 
+		if(girando){
+			Rotar();
+		}
+
 		if (sensor.FrenteAPared()) {
+			girando = true;
 			actuador.Flotar();
 			actuador.Detener();
+			Rotar();
 		} else {
 			actuador.Flotar();
 			actuador.Adelante();
 		}
+
 	}
+
+	void Rotar(){
+		grados++;
+        if(grados <= 90){
+			actuador.Detener();
+			if(dir){
+				actuador.GirarDerecha();
+			}else{
+				actuador.GirarIzquierda();
+			}
+		}else{
+			grados = 0;
+			girando = false;
+		}
+    }
 }
+
