e7c8eec5256ccd0ea128bdcc8bc9f9828cbeaa48
Haans Lopez <jesuslopez@ciencias.unam.mx>
2023-02-28 19:07:40 -0600

    Helices se detienen al finalizar bateria

--- Assets/Scripts/Rotacion.cs
+++ Assets/Scripts/Rotacion.cs
@@ -5,11 +5,28 @@ using UnityEngine;
 public class Rotacion : MonoBehaviour {
 
     public bool rot;
+    private Bateria bateria;
 
-    //Hace rota el GameObject al que está asociado en
+    //Hace rota el GameObject al que estï¿½ asociado en
     //un rango 60 grados por frame.
+    void Start() {
+        bateria = GameObject.Find("Bateria").gameObject.GetComponent<Bateria>();
+    }
+    
+    
     void Update() {
-        this.transform.Rotate(new Vector3(0.0f, 60.0f, 0.0f));
+        if(bateria.NivelDeBateria() <= 0){
+            rot = false;
+        }else{
+            rot = true;
+        }
+
+        if(rot){
+            this.transform.Rotate(new Vector3(0.0f, 60.0f, 0.0f));
+        }
+        else{
+            this.transform.Rotate(new Vector3(0.0f, 0.0f, 0.0f));
+        }
     }
 
 }
